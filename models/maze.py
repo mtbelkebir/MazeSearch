cbe430f4634ca29a81ce302d4a0daa1dad2a1d31
mtbelkebir <mohandtahar.belkebir@se.univ-bejaia.dz>
2024-11-03 18:56:19 +0100

    You're now able to get the neighbour of a node using either the ID or the coordinates

--- models/maze.py
+++ models/maze.py
@@ -27,22 +27,36 @@ class Maze:
     
 
     
-    def get_neighbours(self, pos: tuple[int, int]) -> list[int]:
+    def get_neighbours(self, pos_or_node: int | tuple[int, int]) -> list[int]:
         """
-        Returns the nodes that represent neighbours in final grid as a list. The neighbours are ordered clockwise
-        Adds -1 to the list if there's nothing in a direction
+        Returns the nodes that represent neighbours in final grid as a list. The neighbours are ordered clockwise.
+        Adds -1 to the list if there's nothing in a direction.
+
+        `pos_or_node` can be either a tuple of (x, y) or an integer node ID.
         """
         neighbours = []
-        directions = [(0, -1), (1, 0), (0, 1) , (-1, 0)]
+        directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+
+        if isinstance(pos_or_node, tuple):
+            pos = pos_or_node
+            if not self.position_in_range(pos):
+                raise ValueError(f'Position {pos} out of range of the the grid')
+        elif isinstance(pos_or_node, int):
+            if pos_or_node >= self.adjacency_matrix_length:
+                raise ValueError(f'Node {pos_or_node} is not part of the maze')
+            pos = self.get_position(pos_or_node)
+        else:
+            raise TypeError("Argument must be either a tuple (position) or an integer (node).")
+
         for direction in directions:
             neighbour_pos = (pos[0] + direction[0], pos[1] + direction[1])
-            if self.__position_in_range(neighbour_pos):
-                neighbours.append(self.__get_position(neighbour_pos))
+            if self.position_in_range(neighbour_pos):
+                neighbours.append(self.get_position(neighbour_pos))
             else:
                 neighbours.append(-1)
 
         return neighbours
-    
+
     def __generate_maze(self, starting_cell: int | None = None):
         if starting_cell is None:
             starting_cell = random.randint(0, self.adjacency_matrix_length - 1)
@@ -50,7 +64,7 @@ class Maze:
         visited = {starting_cell}
         while stack:
             current = stack.pop()
-            neighbours = self.__get_neighbours(self.__get_coordinate(current))
+            neighbours = self.get_neighbours(self.get_coordinate(current))
             unvisited_neighbours = [x for x in neighbours if x not in visited and x != -1]
             if  unvisited_neighbours:
                 stack.append(current)
@@ -64,9 +78,6 @@ class Maze:
     def grid(self):
         return self.__adjacency_matrix
 
-    @property
-    def grid_length(self):
-        return self.grid_length
 
     def draw(self, screen_size: tuple[int, int]):
         wall_color = [0, 0, 0]
@@ -76,8 +87,8 @@ class Maze:
         cell_size = screen_width // self.grid_length
 
         for node in range(self.adjacency_matrix_length):
-            x, y = self.__get_coordinate(node)
-            neighbours = self.__get_neighbours((x, y))
+            x, y = self.get_coordinate(node)
+            neighbours = self.get_neighbours((x, y))
             cell_x = x * cell_size
             cell_y = y * cell_size
             cell_x_end = cell_x + cell_size
