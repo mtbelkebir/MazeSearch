1f6b9101fa6016bfcbc5b978169d8c5fa71ac18b
mtbelkebir <mohandtahar.belkebir@se.univ-bejaia.dz>
2024-11-03 22:58:51 +0100

    First implementation of algorithm illustration logic

--- search.py
+++ search.py
@@ -22,5 +22,34 @@ def bfs(maze: Maze):
         for n in unvisited_neigbours:
             visited.add(n)
             queue.append(n)
-
+        colour_maze(maze, visited, screen_size=(1000, 1000))
     pass
+
+
+def colour_maze(maze: Maze, visited: set[int], screen_size: tuple[int, int]):
+    screen_width, _ = screen_size
+    cell_size = screen_width / maze.grid_length
+    gl.glColor3f(0.7, 0, 0.7)
+    for node in visited:
+        x, y = maze.get_coordinate(node)
+        cell_x = x * cell_size
+        cell_y = y * cell_size
+        cell_x_end = cell_x + cell_size
+        cell_y_end = cell_y + cell_size
+
+        l1 = coords_to_glcoords((cell_x, cell_y), screen_size)
+        l2 = coords_to_glcoords((cell_x_end, cell_y), screen_size)
+        l3 = coords_to_glcoords((cell_x_end, cell_y_end), screen_size)
+        l4 = coords_to_glcoords((cell_x, cell_y_end), screen_size)
+
+        gl.glBegin(gl.GL_QUADS)
+
+        gl.glVertex2f(l1[0], l1[1])
+        gl.glVertex2f(l2[0], l2[1])
+        gl.glVertex2f(l3[0], l3[1])
+        gl.glVertex2f(l4[0], l4[1])
+
+        gl.glEnd()
+    maze.draw(screen_size)
+    pygame.time.wait(16)
+    pygame.display.flip()
